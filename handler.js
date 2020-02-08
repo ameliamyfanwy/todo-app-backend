38c1f64711c05506738ee0a388bd75b593fa3a6e
Amelia Lloyd <amelialloyd31@gmail.com>
2020-02-08 17:38:08 +0000

    Updated function to return array of tasks

--- handler.js
+++ handler.js
@@ -1,32 +1,15 @@
 'use strict';
 
 const serverless = require('serverless-http');
-const express = require('express')
+const express = require('express');
 const app = express()
 
-// when someone sends get request to tasks path - want function to fire
-// req = request, res = response
-
 app.get('/tasks', function (req, res) {
-  res.json({
-    message: 'Success'
-  });
+  res.json([
+    {id: 0, description: "adopt dog", category: "Home", completed: false},
+    {id: 1, description: "walk dog", category: "Home", completed: false}
+  ]);
 });
 
 module.exports.tasks = serverless(app);
 
-// module.exports.tasks = async event => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Success!'
-//       },
-//       null,
-//       2
-//     ),
-//   };
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
